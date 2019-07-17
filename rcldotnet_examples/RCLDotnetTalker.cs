b5c97cb4ebaf55c058f2a3b103f71b576e4f0626
Adam Dabrowski <adam.dabrowski@robocartech.com>
2019-07-17 11:05:54 +0200

    removed some unnecessary files and debugs

--- rcldotnet_examples/RCLDotnetTalker.cs
+++ rcldotnet_examples/RCLDotnetTalker.cs
@@ -10,22 +10,15 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Ctor");
             Context ctx = new Context();
-            Console.WriteLine("Ctor1");
             Rclcs.Init(ctx);
-            Console.WriteLine("Ctor2");
             INode node = Rclcs.CreateNode("talker", ctx);
-            Console.WriteLine("Ctor3");
             IPublisher<std_msgs.msg.String> chatter_pub = node.CreatePublisher<std_msgs.msg.String>("chatter");
-            Console.WriteLine("Ctor4");
             std_msgs.msg.String msg = new std_msgs.msg.String();
-            Console.WriteLine("Ctor5");
             int i = 1;
 
             while (Rclcs.Ok(ctx))
             {
-                Console.WriteLine("Ctor6");
                 msg.Data = "Hello World: " + i;
                 i++;
                 Console.WriteLine("Publishing: \"" + msg.Data + "\"");
